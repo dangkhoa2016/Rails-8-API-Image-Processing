c9db6a5847c685188e9748125744896dee29d137
Dang Khoa <i.am@dangkhoa.dev>
2025-03-18 10:21:36 +0000

    update docker

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "color_conversion"
 # you"ve limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Rails8ApiAuthentication
+module Rails8APIImageProcessing
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
